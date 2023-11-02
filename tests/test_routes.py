1dfb9cbfc8e0facdbbc67f902e0e018d87c49ad2
Kjell Reidar Joessang <95139113+Tinnitussen@users.noreply.github.com>
2023-11-02 14:10:26 +0100

    Write tests and rewrite some routes
    - Improve returned status codes for better testability
    - Add some database methods used for testing
    - Add many tests
    - Add some security headers
    - Add zipp as a dependency

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 
 from collections.abc import Iterator
 from typing import TYPE_CHECKING
-
+from flask_login import current_user, login_user
 import pytest
+from io import BytesIO
 
-from app import app
+from app import app, sqlite, User
 
 if TYPE_CHECKING:
     from flask import Flask
     from flask.testing import FlaskClient
-
 
 @pytest.fixture(scope="session")
 def test_app() -> Iterator[Flask]:
@@ -19,6 +19,7 @@ def test_app() -> Iterator[Flask]:
             "SQLITE3_DATABASE": "file::memory:?cache=shared",
             "TESTING": True,
             "WTF_CSRF_ENABLED": False,
+            "SECRET_KEY": "test",
         }
     )
     yield app
@@ -28,7 +29,363 @@ def test_app() -> Iterator[Flask]:
 def client(test_app: Flask) -> FlaskClient:
     return test_app.test_client()
 
+@pytest.fixture()
+def logged_in_client(client: FlaskClient, userid: int = 0) -> FlaskClient:
+    # Valid users should include 0, 1, 2
+    with client:
+        client.post(
+            "/",
+            data = {
+                "login-username": users[userid]["username"],
+                "login-password": users[userid]["password"],
+                "login-submit": "Sign In",
+            }
+        )
+        assert current_user.is_authenticated is True
+    yield client
 
+# Users to test registration and login
+users = [
+    {
+        "description": "New user with valid data",
+        "expected_status_code_registration": 201,
+        "expected_status_code_login": 302,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "test",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with valid data",
+        "expected_status_code_registration": 201,
+        "expected_status_code_login": 302,
+        "first_name": "test1",
+        "last_name": "test1",
+        "username": "test1", 
+        "password": "1test1234",
+    },
+    {
+        "description": "New user with valid data",
+        "expected_status_code_registration": 201,
+        "expected_status_code_login": 302,
+        "first_name": "test2",
+        "last_name": "test2",
+        "username": "test2",
+        "password": "2test1234",
+    },
+    {
+        "description": "New user with duplicate username",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 401,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "test",
+        "password": "wrong_password",
+    },
+    {
+        "description": "New user with too short password",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 400,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "too_short_password",
+        "password": "test",
+    },
+    {
+        "description": "New user with too long password",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 400,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "too_long_password",
+        "password": "test" * 100,
+    },
+    {
+        "description": "New user with too long first name",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 401,
+        "first_name": "test" * 100,
+        "last_name": "test",
+        "username": "too_long_first_name",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with too long last name",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 401,
+        "first_name": "test",
+        "last_name": "test" * 100,
+        "username": "too_long_last_name",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with missing first name",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 401,
+        "first_name": "",
+        "last_name": "test",
+        "username": "missing_first_name",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with missing last name",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 401,
+        "first_name": "test",
+        "last_name": "",
+        "username": "missing_last_name",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with missing username",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 400,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "",
+        "password": "test1234",
+    },
+    {
+        "description": "New user with missing password",
+        "expected_status_code_registration": 400,
+        "expected_status_code_login": 400,
+        "first_name": "test",
+        "last_name": "test",
+        "username": "missing_password",
+        "password": "",
+    },
+]
+
+
+@pytest.mark.parametrize("user", users)
+def test_register(client: FlaskClient, user: dict):
+    with app.app_context():
+        response = client.post(
+            "/",
+            data={
+                "register-first_name": user["first_name"],
+                "register-last_name": user["last_name"],
+                "register-username": user["username"],
+                "register-password": user["password"],
+                "register-confirm_password": user["password"],
+                "register-submit": "Sign Up",
+            }
+        )
+        assert response.status_code == user["expected_status_code_registration"], f"Registration test failed for user: {user['description']}."
+        # Check that the user is in the database for the expected successful registrations
+        if user["expected_status_code_registration"] == 201 or user["description"] == "New user with duplicate username":    
+            assert sqlite.check_user_exists(user["username"]) is True, f"Registration test failed for user: {user['description']}."
+        # Check that the user is not in the database for the expected failures
+        else:
+            assert sqlite.check_user_exists(user["username"]) is False, f"Registration test failed for user: {user['description']}."
+
+@pytest.mark.parametrize("user", users)
+def test_login(client: FlaskClient, user: dict):
+    with client:
+        response = client.post(
+            "/",
+            data={
+                "login-username": user["username"],
+                "login-password": user["password"],
+                "login-submit": "Sign In",
+            }
+        )
+        assert response.status_code == user['expected_status_code_login'], f"Login test failed for user: {user['description']}."
+        # Assert that the user is authenticated for the expected successful logins 
+        if user['expected_status_code_login'] == 302:
+            assert current_user.is_authenticated, f"Login test failed for user: {user['description']}."
+        # Assert that the user is not authenticated for the expected failures
+        else:
+            assert not current_user.is_authenticated, f"Login test failed for user: {user['description']}."
+
+######################## TEST ROUTES GET REQUEST WHILE LOGGED IN ########################
+def test_request_index_logged_in(logged_in_client: FlaskClient):
+    with logged_in_client:
+        response = logged_in_client.get("/")
+        assert response.status_code == 302
+        assert response.location == "/stream/test"
+
+def test_request_index2_logged_in(logged_in_client: FlaskClient):
+    with logged_in_client:
+        response = logged_in_client.get("/index")
+        assert response.status_code == 302
+        # Check that the user is redirected to the stream page
+        assert response.location == "/stream/test"
+
+def test_request_stream_logged_in(logged_in_client: FlaskClient):
+    with logged_in_client:
+        response = logged_in_client.get("/stream/test")
+        assert response.status_code == 200
+
+def test_request_friends_logged_in(logged_in_client: FlaskClient):
+    with logged_in_client:
+        response = logged_in_client.get("/friends/test")
+        assert response.status_code == 200
+
+def test_request_profile_logged_in(logged_in_client: FlaskClient):
+    with logged_in_client:
+        response = logged_in_client.get("/profile/test")
+        assert response.status_code == 200
+
+# Test various post requests to the /stream
+def test_post_stream_valid(logged_in_client: FlaskClient):
+    with app.app_context():
+        with open('test_upload.png', 'rb') as f:
+            image_data = f.read()
+        data = {
+            "content": "test",
+            "image": (BytesIO(image_data), 'test_upload.png'),
+            "submit": "Post",
+        }
+        response = logged_in_client.post("/stream/test", data=data, content_type='multipart/form-data')
+        assert response.status_code == 201
+        # Assert that the image was uploaded 
+        response = logged_in_client.get(f"/uploads/{data['image'][1]}")
+        assert response.status_code == 200
+        # Assert that the post is in the database
+        assert sqlite.check_post_exists(1) is True
+
+def test_post_stream_invalid_filename(logged_in_client: FlaskClient):
+    with app.app_context():
+        with open('virus.exe', 'rb') as f:
+            image_data = f.read()
+        data = {
+            "content": "test",
+            "image": (BytesIO(image_data), 'virus.exe'),
+            "submit": "Post",
+        }
+        response = logged_in_client.post("/stream/test", data=data, content_type='multipart/form-data')
+        assert response.status_code == 400
+        # Assert that the image was not uploaded
+        response = logged_in_client.get(f"/uploads/{data['image'][1]}")
+        assert response.status_code == 404
+        # Assert that the post is not in the database
+        assert sqlite.check_post_exists(2) is False
+
+def test_post_stream_too_long_content(logged_in_client: FlaskClient):
+    with app.app_context():
+        with open('test_upload.png', 'rb') as f:
+            image_data = f.read()
+        data = {
+            "content": "t" * 501,
+            "image": (BytesIO(image_data), 'test_upload2.png'),
+            "submit": "Post",
+        }
+        print(len(data['content']))
+        response = logged_in_client.post("/stream/test", data=data, content_type='multipart/form-data')
+        assert response.status_code == 200
+        # Assert that the image was uploaded 
+        response = logged_in_client.get(f"/uploads/{data['image'][1]}")
+        assert response.status_code == 404
+        # Assert that the post is in the database
+        assert sqlite.check_post_exists(2) is False
+
+def test_post_stream_no_file(logged_in_client: FlaskClient):
+    with app.app_context():
+        data = {
+            "content": "No file",
+            "submit": "Post",
+        }
+        response = logged_in_client.post("/stream/test", data=data, content_type='multipart/form-data')
+        assert response.status_code == 201
+        # Assert that the post is in the database
+        assert sqlite.check_post_exists(2) is True
+
+def test_post_stream_no_content(logged_in_client: FlaskClient):
+    with app.app_context():
+        with open('test_upload2.png', 'rb') as f:
+            image_data = f.read()
+        data = {
+            "image": (BytesIO(image_data), 'test_upload2.png'),
+            "submit": "Post",
+        }
+        response = logged_in_client.post("/stream/test", data=data, content_type='multipart/form-data')
+        assert response.status_code == 200
+        # Assert that the image was not uploaded 
+        response = logged_in_client.get(f"/uploads/{data['image'][1]}")
+        assert response.status_code == 404
+        # Assert that the post is not in the database
+        assert sqlite.check_post_exists(3) is False
+
+###################### COMMENTS ######################
+def test_get_comments_logged_in(logged_in_client: FlaskClient):
+    with app.app_context():
+        response = logged_in_client.get("/comments/test/1")
+        assert response.status_code == 200
+
+def test_get_comments_logged_in_invalid_post_id(logged_in_client: FlaskClient):
+    with app.app_context():
+        response = logged_in_client.get("/comments/test/100")
+        assert response.status_code == 302
+
+def test_get_comments_logged_in_invalid_username(logged_in_client: FlaskClient):
+    with app.app_context():
+        response = logged_in_client.get("/comments/invalid/1")
+        assert response.status_code == 302
+
+def test_post_comments_valid(logged_in_client: FlaskClient):
+    with app.app_context():
+        data = {
+            "comment": "test",
+            "submit": "Comment",
+        }
+        response = logged_in_client.post("/comments/test/1", data=data)
+        assert response.status_code == 201
+        # Assert that the comment is in the database
+        assert sqlite.check_comment_exists(1) is True
+
+def test_post_comments_too_long_content(logged_in_client: FlaskClient):
+    with app.app_context():
+        data = {
+            "comment": "t" * 501,
+            "submit": "Comment",
+        }
+        response = logged_in_client.post("/comments/test/1", data=data)
+        assert response.status_code == 200
+        # Assert that the comment is not in the database
+        assert sqlite.check_comment_exists(2) is False
+
+############## ALL ROUTES WHILE NOT LOGGED IN ##############
 def test_request_index(client: FlaskClient):
     response = client.get("/")
     assert response.status_code == 200
+
+def test_request_index2(client: FlaskClient):
+    response = client.get("/index")
+    assert response.status_code == 200
+
+def test_request_stream(client: FlaskClient):
+    response = client.get("/stream/random")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
+
+def test_request_comments(client: FlaskClient):
+    response = client.get("/comments/random/1")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
+
+def test_request_friends(client: FlaskClient):
+    response = client.get("/friends/random")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
+
+def test_request_profile(client: FlaskClient):
+    response = client.get("/profile/random")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
+
+def test_request_upload(client: FlaskClient):
+    response = client.get("/uploads/random")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
+
+def test_request_logout(client: FlaskClient):
+    response = client.get("/logout")
+    assert response.status_code == 302
+    # Assert that the user is redirected to the login page
+    assert response.location == "/" or response.location == "/index"
